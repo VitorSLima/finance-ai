ffe6e17222a95a0904b52ee950704d157f11dee8
VitorSLima <dev.vitorlima@gmail.com>
2025-07-29 16:34:22 -0400

    feat: add login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} dark antialiased`}
       >
-        <p>layout</p>
         {children}
       </body>
     </html>
